82ce4ac483a3dde8d05a7047c75ed2d12ad36d71
ankitbhuria <upgraderboy@gmail.com>
2024-04-24 20:50:06 +0530

    portfolio ui - v1 mode set to be persistent

--- src/components/mode/Mode.jsx
+++ src/components/mode/Mode.jsx
@@ -1,18 +1,26 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
 function Mode() {
-    const [mode, setMode] = useState("light");
+    const [mode, setMode] = useState(localStorage.getItem("mode") || "light");
     const setDarkMode = () => {
       document.querySelector("body").setAttribute("data-theme", "dark");
+      localStorage.setItem("mode", "dark");
       setMode("dark");
     };
+    useEffect(()=>{
+      if(localStorage.getItem("mode") === "light") setLightMode();
+      else setDarkMode();
+    }, []);
     const setLightMode = () => {
       document.querySelector("body").setAttribute("data-theme", "light");
+      localStorage.setItem("mode", "light");
       setMode("light");
     };
     const toggleTheme = () => {
-      if (mode === "dark") setLightMode();
-      else setDarkMode();
+      if (mode === "dark"){
+        setLightMode();
+      }
+      else {setDarkMode();}
+      console.log(localStorage.getItem("mode"))
     };
   return (
     <>
